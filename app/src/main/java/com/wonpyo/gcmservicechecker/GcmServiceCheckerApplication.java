9d299ee47135e679cc4076d1c3ac7925f6ae96cf
wonpyohong <wonpyo.hong@navercorp.com>
2016-02-11 23:20:50 +0900

    add stetho

---
+++ app/src/main/java/com/wonpyo/gcmservicechecker/GcmServiceCheckerApplication.java
@@ -0,0 +1,17 @@
+package com.wonpyo.gcmservicechecker;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+/**
+ * Created by wonpyohong on 2016. 2. 11..
+ */
+public class GcmServiceCheckerApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Stetho.initializeWithDefaults(this);
+    }
+}
